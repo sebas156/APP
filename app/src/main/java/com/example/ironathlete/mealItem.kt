9412c0347261144c99d96368fad3c1f12b26498f
Sebastian Bonilla <sebocruz@hotmail.com>
2022-03-02 08:42:34 -0500

    Falta terminar el Adapter y falta el setImgaeResource

---
+++ app/src/main/java/com/example/ironathlete/mealItem.kt
@@ -0,0 +1,8 @@
+package com.example.ironathlete
+
+class mealItem (
+    val background: Int,
+    val nameMeal: String,
+    val numberMeal: Int,
+    val profilePhoto: Int
+)
---
+++ app/src/main/java/com/example/ironathlete/ui/diets/DietsAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.ironathlete.ui.diets
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ironathlete.R
+import com.example.ironathlete.databinding.MealCardItemBinding
+import com.example.ironathlete.mealItem
+
+class DietsAdapter (
+    private val dietList: ArrayList<mealItem>
+    ): RecyclerView.Adapter<DietsAdapter.DietViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DietViewHolder {
+        //Tenemos que instanciar una vista.
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.meal_card_item,parent,false)
+        return DietViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DietViewHolder, position: Int) {
+        val meal = dietList[position]
+        holder.bind(meal)
+    }
+
+    override fun getItemCount(): Int = dietList.size
+
+    class DietViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        // ViewHolder se encarga de pintar, acá dentro vamos a colocar la informacion de nuetro ItemView
+        private val binding = MealCardItemBinding.bind(itemView)
+        fun bind(meal: mealItem){
+            with(binding){
+
+            }
+        }
+    }
+}
